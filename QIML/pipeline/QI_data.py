Fixed precision to np.float32 for inputs

QIML/pipeline/QI_data.py
@@ -123,7 +123,7 @@ class QI_H5Dataset_Poisson(QI_H5Dataset):
         E2 = self.E2[0]
         vis = self.vis[0]
 
-        x = np.zeros((self.nframes, *y.shape))
+        x = np.zeros((self.nframes, *y.shape), dtype=np.float32)
         for i in range(0, self.nframes):
             phi      = np.random.rand(1)[0]*2*np.pi  # random phase offset
             phase    = y + phi
@@ -224,10 +224,10 @@ if __name__ == '__main__':
 
     import time
     from matplotlib import pyplot as plt
-    from torch import nn
+
     # data_fname = 'image_data_n10_nbar10000_nframes16_npix32.h5'
     # data_fname = 'QI_devset.h5'
-    data_fname = 'QIML_poisson_data_n100_npix64.h5'
+    data_fname = 'QIML_poisson_data_n666_npix64.h5'
 
     data = QIDataModule(data_fname, batch_size=10, nbar=1e4, nframes=64)
     data.setup()
@@ -235,5 +235,7 @@ if __name__ == '__main__':
     start = time.time()
     (x, y) = data.train_set.__getitem__(1)
     print(f'Time: {time.time() - start}')
+
+
 
     print('fin')

data/raw/make_dataset_for_poisson.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 from QIML.utils import random_rotate_image, random_roll_image, convertGreyscaleImgToPhase
 
 
-ndata   = 1000 # number of different training frame sets to include in a data set
+ndata   = 666 # number of different training frame sets to include in a data set
 nx      = 64 # X pixels
 ny      = 64 # Y pixels
 sigma_X = 100
@@ -27,8 +27,10 @@ y = np.linspace(-5, 5, ny)
 X, Y = np.meshgrid(x, y)
 
 # the beam profile generation could also be moved to the data generation loop, so that different beam profiles are used in the training. Same for visibility
-E1 = exp(-(X)**2/(2*sigma_X**2) - (Y)**2/(2*sigma_Y**2))
-E2 = exp(-(X)**2/(2*sigma_X**2) - (Y)**2/(2*sigma_Y**2))
+E1 = np.exp(-(X)**2/(2*sigma_X**2) - (Y)**2/(2*sigma_Y**2))
+E2 = np.exp(-(X)**2/(2*sigma_X**2) - (Y)**2/(2*sigma_Y**2))
+E1 = E1.astype(np.float32)
+E2 = E2.astype(np.float32)
 
 """ Data generation loop """
 truths_data = np.zeros((ndata, nx, ny), dtype=np.float32)
@@ -49,4 +51,4 @@ with h5py.File(basepath+filepath, "a") as h5_data:
     h5_data["inputs"] = []
     h5_data["E1"] = np.array([E1])
     h5_data["E2"] = np.array([E2])
-    h5_data["vis"] = np.array([vis])
+    h5_data["vis"] = np.array([vis], dtype=np.float32)