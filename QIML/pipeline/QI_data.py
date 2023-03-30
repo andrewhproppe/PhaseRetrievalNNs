Fixed broadcasting and normalization for __getitem__

QIML/pipeline/QI_data.py
@@ -11,6 +11,10 @@ import pytorch_lightning as pl
 from QIML.pipeline import transforms
 from QIML.utils import paths
 
+import platform
+import matplotlib as mpl
+if platform.system()=='Linux':
+    mpl.use('TkAgg')
 
 class QI_H5Dataset(Dataset):
     def __init__(self, filepath: str, seed: int = 10236, **kwargs):
@@ -119,15 +123,17 @@ class QI_H5Dataset_Poisson(QI_H5Dataset):
         y : torch.Tensor
             Noise-free phase mask
         """
-        y = torch.tensor(self.truths[index])
-        E1 = torch.tensor(self.E1[0])
-        E2 = torch.tensor(self.E2[0])
-        vis = torch.tensor(self.vis[0])
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        y = torch.tensor(self.truths[index]).to(device)
+        E1 = torch.tensor(self.E1[0]).to(device)
+        E2 = torch.tensor(self.E2[0]).to(device)
+        vis = torch.tensor(self.vis[0]).to(device)
 
         """ Make Poisson sampled frames through only broadcasted operations. Seems about 30% faster on CPU """
         phi        = torch.rand(self.nframes)*2*torch.pi
-        phase_mask = y.repeat(self.nframes, 1, 1)
-        phase      = phase_mask + phi.unsqueeze(-1).unsqueeze(-1)
+        phase_mask = y.repeat(self.nframes, 1, 1).to(device)
+        phase      = phase_mask + phi.unsqueeze(-1).unsqueeze(-1).to(device)
         I          = torch.abs(E1)**2+torch.abs(E2)**2 + 2*vis*torch.abs(E1)*torch.abs(E2)*torch.cos(phase)
         I_maxima   = torch.sum(I, axis=(-2, -1)).unsqueeze(-1).unsqueeze(-1)
         I          = I*self.nbar/I_maxima
@@ -148,8 +154,9 @@ class QI_H5Dataset_Poisson(QI_H5Dataset):
         # # x[i, :, :] = np.random.poisson(I_scaled)
         # x = torch.poisson(x)
 
-        x = self.input_transform(x)
-        y = self.truth_transform(y)
+        # TODO: Figure out normalization for images
+        x = self.input_transform(x).to(torch.device('cpu'))
+        y = self.truth_transform(y).to(torch.device('cpu'))
 
         return x, y
 
@@ -246,7 +253,7 @@ if __name__ == '__main__':
 
     # data_fname = 'image_data_n10_nbar10000_nframes16_npix32.h5'
     # data_fname = 'QI_devset.h5'
-    data_fname = 'QIML_poisson_data_n666_npix64.h5'
+    data_fname = 'QIML_nhl_poisson_data_n666_npix64.h5'
 
     data = QIDataModule(data_fname, batch_size=10, nbar=1e4, nframes=64)
     data.setup()

QIML/pipeline/transforms.py
@@ -16,6 +16,12 @@ class Normalize(object):
         return ((y - y.min()) / np.nanmax([y.max() - y.min(), 1e-7]))
         # return (y) / np.nanmax([y.max(), 1e-7])
 
+class TorchNormalize(object):
+    def __call__(self, y: torch.Tensor):
+        y = y - torch.min(y)
+        y = y / torch.max(y)
+        return y
+
 
 class AddChannelDim(object):
     def __init__(self, dim):
@@ -280,7 +286,8 @@ def input_transform_pipeline():
     """
     pipeline = Compose(
         [
-            Normalize(),
+            TorchNormalize(),
+            # Normalize(),
             # ArrayToTensor(),
         ]
     )
@@ -306,7 +313,8 @@ def truth_transform_pipeline(*args):
     """
     pipeline = Compose(
         [
-            Normalize(),
+            TorchNormalize(),
+            # Normalize(),
             # ArrayToTensor(),
         ]
     )

QIML/visualization/AP_figs_funcs.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib.ticker import AutoMinorLocator
 
 c      = 299792458  # m/s
-π      = 3.141592653589793
+π      = np.pi
 
 # import seaborn as sns
 # base_colors = np.array(sns.color_palette("icefire", 12))

data/raw/make_dataset_2dimPhase_gst.py
@@ -81,8 +81,8 @@ outcome_list = np.array(outcome_list).copy() # create a array of possible indice
 
 """ Data generation loop """
 
-# png training images should in a folder called masks (in same directory as script)
-filenames = next(walk('../masks'), (None, None, []))[2] # directory of phase mask .png files
+# png training images should in a folder called masks_nhl (in same directory as script)
+filenames = next(walk('../masks_nhl'), (None, None, []))[2] # directory of phase mask .png files
 
 inputs_data = np.zeros((ndata, num_frames, nx, ny))
 truths_data = np.zeros((ndata, nx, ny))
@@ -92,7 +92,7 @@ for d in tqdm(range(0, ndata)):
     idx = random.randint(0, len(filenames)-1)
     mask = filenames[idx]
     
-    filename = '../masks/'+mask
+    filename = '../masks_nhl/'+mask
     image = np.array(Image.open(filename).convert('L')) # load as greyscale
     phase_mask = image * 1.0 / 255 * 2 * np.pi # convert to phase
     phase_mask = np.fliplr(np.flip(phase_mask)) # flip so that upright

data/raw/make_dataset_for_poisson.py
@@ -3,24 +3,28 @@ import numpy as np
 import random
 import imageio
 import matplotlib as mpl
+import os
 
 from matplotlib import pyplot as plt
-from os import walk
 from numpy import exp, pi
 from skimage.transform import resize
 from tqdm import tqdm
 from QIML.utils import random_rotate_image, random_roll_image, convertGreyscaleImgToPhase
 
 
-ndata   = 666 # number of different training frame sets to include in a data set
+ndata   = 2000 # number of different training frame sets to include in a data set
 nx      = 64 # X pixels
 ny      = 64 # Y pixels
 sigma_X = 100
 sigma_Y = 100
 vis     = 1
 
-filenames = next(walk('../masks'), (None, None, []))[2] # directory of phase mask .png files
-filenames.pop(0)
+# import matplotlib.image as mpimg
+# image = mpimg.imread('../masks_nhl/bruins.png')
+
+masks_folder = 'masks_nhl'
+# masks_folder = 'masks'
+filenames = os.listdir('../'+masks_folder)
 
 x = np.linspace(-5, 5, nx)
 y = np.linspace(-5, 5, ny)
@@ -37,14 +41,14 @@ truths_data = np.zeros((ndata, nx, ny), dtype=np.float32)
 for d in tqdm(range(0, ndata)):
     idx = random.randint(0, len(filenames)-1)
     mask = filenames[idx]
-    phase_mask = np.fliplr(np.flip(convertGreyscaleImgToPhase('../masks/'+mask, nx, ny)))
+    phase_mask = np.fliplr(np.flip(convertGreyscaleImgToPhase('../'+masks_folder+'/'+mask, nx, ny)))
     phase_mask = random_rotate_image(phase_mask)
     phase_mask = random_roll_image(phase_mask)
     truths_data[d, :, :] = phase_mask # frames seem to always be inverted compared to the original image
 
 """ Save the data to .h5 file """
 basepath = ""
-filepath = 'QIML_poisson_data_n%i_npix%i.h5' % (ndata, nx)
+filepath = 'QIML_nhl_poisson_data_n%i_npix%i.h5' % (ndata, nx)
 
 with h5py.File(basepath+filepath, "a") as h5_data:
     h5_data["truths"] = truths_data