f2fb04cafd1cd10e2c1d94057c2a7e2c768bb3d1
Dr. Andrew H. Proppe <andrew.proppe@gmail.com>
2023-08-10 13:44:49 -0400

    Updating prediction comparison scripts and some visualizations

---
+++ scripts/old/test_data_simulation.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+import torch
+from matplotlib import pyplot as plt
+
+# from QIML.models.utils import SSIM
+# from torch.nn import MSELoss
+# from QIML.models.QI_models import SRN3D_v3
+from QIML.pipeline.transforms import input_transform_pipeline
+
+# from utils import compute_svd_loss, compute_model_loss, save_pickle_with_auto_increment
+
+from QIML.visualization.visualize import plot_frames
+import h5py
+
+# import matplotlib as mpl
+# mpl.use("TkAgg")
+
+if __name__ == "__main__":
+    # Load experimental data set
+    root = "../data/expt"
+    date = "20230808"
+    data_fname = "raw_frames_0.05ms.npy"
+    bg_fname = "bg_frames_0.05ms.npy"
+    data = np.load(f"{root}/{date}/{data_fname}").astype(np.float32)
+    bg = np.load(f"{root}/{date}/{bg_fname}").astype(np.float32)
+    # transforms = input_transform_pipeline()
+    # x_expt = transforms(torch.tensor(data[0, :, :, :]))
+    idx = 4
+    x_expt = data[idx, :, :, :]
+    bg_expt = bg[idx, :, :, :]
+    plot_frames(x_expt, nrows=4, figsize=(4, 4), dpi=150, cmap="viridis")
+    # plot_frames(bg_expt, nrows=3, figsize=(4, 4), dpi=150, cmap="viridis")
+    # plot_frames(x_expt - bg_expt, nrows=3, figsize=(4, 4), dpi=150, cmap="viridis")
+    del data, bg
+
+    # raise RuntimeError
+
+    # Simulate experimental data
+    path = "/data/raw/flowers_n5000_npix64.h5"
+    masks = h5py.File(path, "r")["truths"][0:100, :, :]
+    E1 = torch.Tensor(h5py.File(path, "r")["E1"][0])
+    E2 = E1
+    vis = 1
+    nbar_signal = 0.5e5
+    nbar_bkgrnd = 1.3e6
+    npixels = 64 * 64
+
+    nframes = 32
+    y = torch.Tensor(masks[idx, :, :])
+    phi = torch.rand(32) * 2 * torch.pi  # generate array of phi values
+    phase_mask = y.repeat(nframes, 1, 1)
+    phase_mask = phase_mask + phi.unsqueeze(-1).unsqueeze(-1)
+    x = (
+        torch.abs(E1) ** 2
+        + torch.abs(E2) ** 2
+        + 2 * vis * torch.abs(E1) * torch.abs(E2) * torch.cos(phase_mask)
+    )
+    x = x / x[0, :, :].sum()
+
+    # raise RuntimeError
+
+    x = x * nbar_signal
+    x = x + nbar_bkgrnd / npixels
+    x = torch.poisson(x)
+    plot_frames(x, nrows=4, figsize=(4, 4), dpi=150, cmap="viridis")
+    raise RuntimeError
+
+    factor = 20
+    bckgrnd = 0.5
+    x = torch.poisson(bckgrnd * factor + factor * x)
+    x = transforms(x)
+    plt.imshow(x[1, :, :])
+
+    # xmin = torch.amin(x, dim=(1, 2))
+    # x = x - xmin.unsqueeze(-1).unsqueeze(-1)
+
+    #
+    # x_maxima = torch.sum(x, axis=(-2, -1)).unsqueeze(-1).unsqueeze(-1)
+    # scale = nbar / x[0, :, :].sum()
+    # x = x * scale
+    # x = x + 5 * scale
+    # x = torch.poisson(x)
+
+    plot_frames(x, nrows=45, figsize=(4, 4), dpi=150, cmap="viridis")
+    # plt.imshow(torch.poisson(300 + 100 * y / y.max()))
+
+    raise RuntimeError
+
+    transforms = input_transform_pipeline()
+    idx = 0
+    x = transforms(torch.tensor(data[idx, :, :, :]).unsqueeze(0))
+    # bckgrnd = torch.mean(x, dim=1)
+    # x -= bckgrnd.unsqueeze(1)
+
+    # Load trained model and set to eval
+    model = SRN3D_v3.load_from_checkpoint("../trained_models/SRN3Dv3_optim.ckpt").cuda()
+    model.eval()
+
+    y, z = model(x.cuda())
+    y = y.squeeze(0).cpu().detach().numpy()
+
+    cmap = "viridis"
+    fig, ax = plt.subplots(1, 2, figsize=(6, 2), dpi=150)
+    ax[0].imshow(x[0, 0, :, :], cmap=cmap)
+    ax[1].imshow(y[:, :], cmap=cmap)
--- scripts/predict_expt.py
+++ scripts/predict_expt.py
@@ -3,100 +3,71 @@ import numpy as np
 import torch
 from matplotlib import pyplot as plt
 
-# from QIML.models.utils import SSIM
-# from torch.nn import MSELoss
-# from QIML.models.QI_models import SRN3D_v3
+from QIML.models.utils import SSIM
+from torch.nn import MSELoss
+from QIML.models.QI_models import SRN3D_v3
 from QIML.pipeline.transforms import input_transform_pipeline
+from utils import norm_to_phase, phase_to_norm, frames_to_svd
 
-# from utils import compute_svd_loss, compute_model_loss, save_pickle_with_auto_increment
+from utils import compute_svd_loss, compute_model_loss, save_pickle_with_auto_increment
 
 from QIML.visualization.visualize import plot_frames
+from QIML.visualization.AP_figs_funcs import *
 import h5py
 
-# import matplotlib as mpl
-# mpl.use("TkAgg")
+
+def load_expt_data(acq_time, date, idx=None, root="../data/expt"):
+    data_fname = f"raw_frames_{acq_time}ms.npy"
+    bg_fname = f"bg_frames_{acq_time}ms.npy"
+    y_true_fname = f"theory_phase_{acq_time}ms.npy"
+    data = np.load(f"{root}/{date}/{data_fname}").astype(np.float32)
+    bg = np.load(f"{root}/{date}/{bg_fname}").astype(np.float32)
+    y_true = np.load(f"{root}/{date}/{y_true_fname}").astype(np.float32)
+    if idx is not None:
+        data = data[idx, :, :, :]
+        bg = bg[idx, :, :, :]
+        y_true = y_true[idx, :, :]
+    return data, y_true, bg
+
+
+def pick_phi(phi1, phi2, y_true):
+    l1 = torch.nn.L1Loss()(phi1, torch.Tensor(y_true))
+    l2 = torch.nn.L1Loss()(phi2, torch.Tensor(y_true))
+    phi, l = (phi1, l1) if l1 < l2 else (phi2, l2)
+    return phi
+
 
 if __name__ == "__main__":
     # Load experimental data set
-    root = "../data/expt"
-    date = "20230808"
-    data_fname = "raw_frames_0.1ms.npy"
-    bg_fname = "bg_frames_0.1ms.npy"
-    data = np.load(f"{root}/{date}/{data_fname}").astype(np.float32)
-    bg = np.load(f"{root}/{date}/{bg_fname}").astype(np.float32)
-    # transforms = input_transform_pipeline()
-    # x_expt = transforms(torch.tensor(data[0, :, :, :]))
-    x_expt = data[0, :, :, :]
-    bg_expt = bg[0, :, :, :]
-    plot_frames(x_expt, nrows=3, figsize=(4, 4), dpi=150, cmap="viridis")
-    # plot_frames(bg_expt, nrows=3, figsize=(4, 4), dpi=150, cmap="viridis")
-    # plot_frames(x_expt - bg_expt, nrows=3, figsize=(4, 4), dpi=150, cmap="viridis")
-    del data, bg
-
-    # raise RuntimeError
-
-    # Simulate experimental data
-    path = "/Users/andrewproppe/Library/CloudStorage/GoogleDrive-andrew.proppe@gmail.com/My Drive/python/QuantumImagingML/data/raw/flowers_n5000_npix64.h5"
-    masks = h5py.File(path, "r")["truths"][0:100, :, :]
-    E1 = torch.Tensor(h5py.File(path, "r")["E1"][0])
-    E2 = E1
-    vis = 1
-    nbar_signal = 1e5
-    nbar_bkgrnd = 1e6
-    npixels = 64 * 64
-
-    nframes = 32
-    y = torch.Tensor(masks[0, :, :])
-    phi = torch.rand(32) * 2 * torch.pi  # generate array of phi values
-    phase_mask = y.repeat(nframes, 1, 1)
-    phase_mask = phase_mask + phi.unsqueeze(-1).unsqueeze(-1)
-    x = (
-        torch.abs(E1) ** 2
-        + torch.abs(E2) ** 2
-        + 2 * vis * torch.abs(E1) * torch.abs(E2) * torch.cos(phase_mask)
-    )
-    x = x / x[0, :, :].sum()
-    x = x * nbar_signal
-    x = x + nbar_bkgrnd / npixels
-    x = torch.poisson(x)
-    plot_frames(x, nrows=3, figsize=(4, 4), dpi=150, cmap="viridis")
-    raise RuntimeError
-
-    factor = 20
-    bckgrnd = 0.5
-    x = torch.poisson(bckgrnd * factor + factor * x)
-    x = transforms(x)
-    plt.imshow(x[1, :, :])
-
-    # xmin = torch.amin(x, dim=(1, 2))
-    # x = x - xmin.unsqueeze(-1).unsqueeze(-1)
-
-    #
-    # x_maxima = torch.sum(x, axis=(-2, -1)).unsqueeze(-1).unsqueeze(-1)
-    # scale = nbar / x[0, :, :].sum()
-    # x = x * scale
-    # x = x + 5 * scale
-    # x = torch.poisson(x)
-
-    plot_frames(x, nrows=5, figsize=(4, 4), dpi=150, cmap="viridis")
-    # plt.imshow(torch.poisson(300 + 100 * y / y.max()))
-
-    raise RuntimeError
+    x, y_true, bkgrnd = load_expt_data(acq_time=0.1, idx=10, date="20230808")
+    y_true -= torch.pi
 
     transforms = input_transform_pipeline()
-    idx = 0
-    x = transforms(torch.tensor(data[idx, :, :, :]).unsqueeze(0))
-    # bckgrnd = torch.mean(x, dim=1)
-    # x -= bckgrnd.unsqueeze(1)
+    x = transforms(torch.tensor(x))
 
     # Load trained model and set to eval
-    model = SRN3D_v3.load_from_checkpoint("../trained_models/SRN3Dv3_optim.ckpt").cuda()
+    model = SRN3D_v3.load_from_checkpoint("../trained_models/SRN3D_bg1.ckpt")
     model.eval()
 
-    y, z = model(x.cuda())
+    # Get model prediction
+    y, z = model(x.unsqueeze(0))
     y = y.squeeze(0).cpu().detach().numpy()
+    y = norm_to_phase(y)
 
-    cmap = "viridis"
-    fig, ax = plt.subplots(1, 2, figsize=(6, 2), dpi=150)
-    ax[0].imshow(x[0, 0, :, :], cmap=cmap)
-    ax[1].imshow(y[:, :], cmap=cmap)
+    # Get SVD prediction
+    phi1, phi2 = frames_to_svd(norm_to_phase(x))
+    phi = pick_phi(phi1, phi2, y_true)
+
+    set_font_size(8)
+    cmap = "hsv"
+    fig, ax = plt.subplots(1, 4, figsize=(8, 4), dpi=150)
+    ax = ax.flatten()
+    ax[0].imshow(x[0, :, :], cmap=cmap)
+    ax[0].set_title("Input")
+    ax[1].imshow(y_true[:, :], cmap=cmap)
+    ax[1].set_title("True")
+    ax[2].imshow(y[:, :], cmap=cmap)
+    ax[2].set_title("NN")
+    ax[3].imshow(phi[:, :], cmap=cmap)
+    ax[3].set_title("SVD")
+    dress_fig(legend=False)
