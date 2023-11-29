b0019c3cf87019fa9812490ea8d795bf3b6338fb
Dr. Andrew H. Proppe <andrew.proppe@gmail.com>
2023-11-29 09:56:03 -0500

    Testing Git pull to GPU

--- scripts/versus_shot_noise.py
+++ scripts/versus_shot_noise.py
@@ -1,48 +1,53 @@
 import torch
+
 from PRNN.models.base import PRUNe
 from PRNN.pipeline.image_data import make_interferogram_frames
 from PRNN.utils import get_system_and_backend
 from PRNN.visualization.figure_utils import *
 from PRNN.pipeline.transforms import input_transform_pipeline
 from data.utils import get_from_h5
+from tqdm import tqdm
 
 get_system_and_backend()
 
 model = PRUNe.load_from_checkpoint(
     checkpoint_path="../trained_models/bkgd_free/jolly-cloud-1.ckpt",
-    # map_location=torch.device("gpu")
+    map_location=torch.device("cpu")
 ).eval()
 
 transforms = input_transform_pipeline()
 mse = torch.nn.MSELoss()
+nsamples = 10
 nbar = 1e3
+nframes = 32
+npixels = 64
 
 y, E1, E2, vis = get_from_h5("../data/raw/flowers_n5000_npix64.h5", 2, model.device)
-y = (y - y.min()) / (y.max() - y.min())
 
-# plt.imshow(x[0, :, :].cpu().numpy(), cmap="gray")
+# Normalize y
+# y = (y - y.min()) / (y.max() - y.min())
+y = y / y.max()
+
+# Generate predictions
 mse_list = []
-yhats = []
-for i in range(0, 5):
+yhat_list = []
+for i in tqdm(range(0, nsamples)):
     with torch.no_grad():
-        x = make_interferogram_frames(y, E1, E2, vis, nbar, 0, 64, 32, model.device)
+        x = make_interferogram_frames(y, E1, E2, vis, nbar, 0, npixels, nframes, model.device)
         x = transforms(x)
         yhat, _ = model(x.unsqueeze(0))
-        yhats.append(yhat)
+        yhat_list.append(yhat.squeeze(0))
 
+# Scale NN outputs to have nbar photons each
+for i in range(len(yhat_list)):
+    yhat_list[i] = yhat_list[i] / yhat_list[i].sum() * nbar
 
-def shot_noise_limit_uncertainty(total_photon_counts, image_size):
-    # Calculate mean photon count per pixel
-    mean_photon_count = total_photon_counts / (image_size[0] * image_size[1])
-    # Calculate shot noise limit (standard deviation)
-    shot_noise_limit = torch.sqrt(torch.tensor(mean_photon_count))
-    return shot_noise_limit
+yhat = torch.stack(yhat_list, dim=0).squeeze(1)
+NN_μ = torch.mean(yhat, dim=0)
+NN_σ = torch.std(yhat, dim=0)
 
-
-        # mse_list.append(mse(yhat.squeeze(0), y))
-
-# plt.figure()
-# plt.imshow(yhat[0, :, :].cpu(), cmap="twilight_shifted")
-#
-# plt.figure()
-# plt.imshow(y.cpu(), cmap="twilight_shifted")
+# Multiply y by total number of photons in all frames (nframes * nbar)
+scaled_y = y / y.sum() * nframes * nbar
+SNL = 1/torch.sqrt(scaled_y)
+QNL = 1/scaled_y
+# SNL_σ = y / y.sum() * nframes * nbar
